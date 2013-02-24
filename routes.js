249cfaf33b9cf65235f0a8f39b37a9997f19b369
Yuval Dekel <thedekel@gmail.com>
2013-02-23 22:35:47 -0500

    games:false

--- routes.js
+++ routes.js
@@ -31,7 +31,7 @@ module.exports.main_page = function(req, res){
     });
   } else {
     //return not-logged in
-    return res.render("index", {app:{id:process.env.FACEBOOK_APP_ID}});
+    return res.render("index", {app:{id:process.env.FACEBOOK_APP_ID, games: false}});
   }
 };
 
