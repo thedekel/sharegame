aa71c96617dea4ef4ef076960a5d442651bf639f
Yuval Dekel <thedekel@gmail.com>
2013-02-24 01:51:57 -0500

    fixed, modified and :shipit:

--- routes.js
+++ routes.js
@@ -1,4 +1,5 @@
 var Mongolian = require("mongolian");
+var request = require('request');
 var ObjectId = Mongolian.ObjectId;
 
 var db;
@@ -12,6 +13,7 @@ if (process.env.MONGOLAB_URI){
 
 var games = db.collection("games");
 var users = db.collection("users");
+var purchases = db.collection("users");
 
 /* MIN VIABLE
  * GET /
@@ -62,8 +64,35 @@ module.exports.buy_request_page = function(req, res){
  * create a new purchase on the database, notify target user
  */
 module.exports.submit_buy_request = function(req, res){
-
-  res.send('create a new purchase on thedatabase, notify target user');
+  var game_id = req.params.game_id;
+  req.facebook.me(function(me){
+    var friend_id = req.body.friend_id,
+    auction_link = req.body.auction_link,
+    auction_price = req.body.auction_price,
+    auction_pic = req.body.auction_pic,
+    auction_title = req.body.auction_title,
+    game_title = req.body.game_title,
+    game_id = req.body.game_id;
+    var new_p = {
+      origin: me,
+      friend: friend_id,
+      auction: {
+        link: auction_link,
+        price: auction_price,
+        pic:auction_pic,
+        game: game_title,
+        game_id: game_id,
+        title: auction_title
+      }
+    };
+    purchases.insert(new_p, function(err, doc){
+      request.post("https://graph.facebook.com/" + friend_id + 
+        "/notifications?acces_token=" + req.facebook.token + "&href=/purchase/accept/" + doc._id + 
+        "&template=@[" + me.id + "] wants to buyy " + game_title + " with you!", function(e, r, body){
+        res.redirect("/?msg=your%20purchase%20has%20been%20submitted");
+      });
+    });
+  });
 };
 
 /* MIN VIABLE
@@ -71,7 +100,9 @@ module.exports.submit_buy_request = function(req, res){
  * page where a user is asked to accept or decline a purchase offer
  */
 module.exports.accept_purchase_page = function(req,res){
-  res.send('page where a user is asked to accept or decline a purchase offer');
+  purchases.findOne({_id:new ObjectId(req.params.purchase_id)}, function(err, pur){
+    res.send(JSON.stringify(pur));
+  });
 };
 
 /* MIN VIABLE
